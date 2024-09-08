11a886e41c44acf7be18b9fbfa86fba25a83918a
Badreddine sarrour <badr55sarour@gmail.com>
2024-09-08 20:54:48 +0100

    first commit

---
+++ components/app.jsx
@@ -0,0 +1,9 @@
+import Body from "./body"
+
+export default function App(){
+    return(
+        <>
+            <Body/>
+        </>
+    )
+}
---
+++ components/body.jsx
@@ -0,0 +1,50 @@
+
+export default function Body(){
+    return(
+        <>
+            <div className="body-container">
+
+                /* Navbar */
+                <nav>
+                    <ul>
+                        <button>About</button>
+                        <button>Resume</button>
+                        <button>Portfolio</button>
+                        <button>Blog</button>
+                        <button>Contact</button>
+                    </ul>
+                </nav>
+
+                /* Body */
+                <h2>About Me</h2>
+                <section>
+                    <p>
+                    At Raytheon, I’ve developed a strong foundation in cybersecurity, focusing on
+                    penetration testing, firewall configuration, and IDS setup.
+                    I was responsible for implementing security policies and responding swiftly to data security crises,
+                    ensuring the protection of corporate infrastructure.
+
+                    In addition to cybersecurity, I managed virtual machines, user accounts, and network equipment,
+                    while configuring server hardware and maintaining VMWare infrastructure.
+                    My work involved optimizing system performance and ensuring high availability across IT systems.
+
+                    I am proficient in Java, Python, and C++, with experience in scripting. During my internship at Harvard,
+                    I applied these programming skills to Quantum Computing projects, solving algorithms and research problems.
+                    Through my master’s coursework, I’ve also led large-scale IT projects,
+                    including application deployment and building cybersecurity programs,
+                    with a focus on cloud computing and project planning.
+                    </p>
+                </section>
+
+                /* Add bellow the serivce and what you can provide */
+                <section>
+                    <h3>what I'm Doing</h3>
+                    <ul>
+                        <li></li>
+                    </ul>
+                </section>
+
+            </div>
+        </>
+    )
+}
